affbdcaa5edbcf11d676795c8522f4d47311c830
IndianBartonka <bartonka2017@outlook.com>
2024-02-23 14:20:46 +0100

    Implement custom role icon

--- DiscordExtension/src/main/java/me/indian/discord/config/sub/LinkingConfig.java
+++ DiscordExtension/src/main/java/me/indian/discord/config/sub/LinkingConfig.java
@@ -2,6 +2,8 @@ package me.indian.discord.config.sub;
 
 import eu.okaeri.configs.OkaeriConfig;
 import eu.okaeri.configs.annotation.Comment;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LinkingConfig extends OkaeriConfig {
 
@@ -26,6 +28,29 @@ public class LinkingConfig extends OkaeriConfig {
     @Comment({"Czy użytkownik może sam rozłączyć swoje konto Discord z kontem Minecraft?"})
     private boolean canUnlink = false;
 
+    @Comment({""})
+    @Comment({"Własne role, jak to wygląda?"})
+    @Comment({"ID ROLI : IKONA"})
+    @Comment({"Pozwala to na wyświetlenie custom rang"})
+    private Map<Long, String> customRoles = new LinkedHashMap<>();
+
+    @Comment({""})
+    @Comment({"Czy używać custom roli"})
+    private boolean useCustomRoles = true;
+
+    @Comment({""})
+    @Comment({"Czy używać tylko custom roli"})
+    private boolean onlyCustomRoles = false;
+
+    public LinkingConfig() {
+        if (this.customRoles.isEmpty()) {
+            this.customRoles.put(1204425453109121054L, "\uE400");
+            this.customRoles.put(925851131698098226L, "\uE400");
+            this.customRoles.put(927545485512826880L, "\uE401");
+            this.customRoles.put(925793764822368286L, "\uE405");
+        }
+    }
+
     public long getLinkedRoleID() {
         return this.linkedRoleID;
     }
@@ -44,5 +69,17 @@ public class LinkingConfig extends OkaeriConfig {
 
     public boolean isCanUnlink() {
         return this.canUnlink;
+    }
+
+    public Map<Long, String> getCustomRoles() {
+        return this.customRoles;
+    }
+
+    public boolean isUseCustomRoles() {
+        return this.useCustomRoles;
+    }
+
+    public boolean isOnlyCustomRoles() {
+        return this.onlyCustomRoles;
     }
 }
--- DiscordExtension/src/main/java/me/indian/discord/listener/PlayerEventListener.java
+++ DiscordExtension/src/main/java/me/indian/discord/listener/PlayerEventListener.java
@@ -10,27 +10,28 @@ import me.indian.bds.event.player.response.PlayerChatResponse;
 import me.indian.bds.util.DateUtil;
 import me.indian.discord.DiscordExtension;
 import me.indian.discord.config.MessagesConfig;
+import me.indian.discord.config.sub.LinkingConfig;
 import me.indian.discord.embed.component.Footer;
 import me.indian.discord.jda.DiscordJDA;
 import me.indian.discord.jda.manager.LinkingManager;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 
 public class PlayerEventListener extends Listener {
 
-    private final DiscordExtension discordExtension;
     private final BDSAutoEnable bdsAutoEnable;
     private final MessagesConfig messagesConfig;
     private final DiscordJDA discordJDA;
     private final LinkingManager linkingManager;
+    private final LinkingConfig linkingConfig;
 
     public PlayerEventListener(final DiscordExtension discordExtension) {
-        this.discordExtension = discordExtension;
-        this.bdsAutoEnable = this.discordExtension.getBdsAutoEnable();
-        this.messagesConfig = this.discordExtension.getMessagesConfig();
-        this.discordJDA = this.discordExtension.getDiscordJDA();
+        this.bdsAutoEnable = discordExtension.getBdsAutoEnable();
+        this.messagesConfig = discordExtension.getMessagesConfig();
+        this.discordJDA = discordExtension.getDiscordJDA();
         this.linkingManager = this.discordJDA.getLinkingManager();
+        this.linkingConfig = discordExtension.getConfig().getBotConfig().getLinkingConfig();
     }
-
 
     @Override
     public void onPlayerJoin(final PlayerJoinEvent event) {
@@ -61,15 +62,14 @@ public class PlayerEventListener extends Listener {
             return null;
         }
 
-        //TODO: Dodać mape "ID ROLI : IKONA" aby wyświetlać nie standardowe rangi na czacie 
         boolean memberMutedOnDiscord = false;
         String role = "";
 
         if (!event.isMuted() && this.messagesConfig.isFormatChat() && this.linkingManager.isLinked(playerName)) {
             final Member member = this.linkingManager.getMember(playerName);
             if (member != null) {
-                role = this.discordJDA.getColoredRole(this.discordJDA.getHighestRole(member.getIdLong()));
                 memberMutedOnDiscord = member.isTimedOut();
+                role = this.getRole(member);
             }
         }
 
@@ -107,5 +107,37 @@ public class PlayerEventListener extends Listener {
                 .replaceAll("§l", "**")
                 .replaceAll("§r", "")
         );
+    }
+
+    private String getRole(final Member member) {
+        final Role highestRole = this.discordJDA.getHighestRole(member.getIdLong());
+        if (highestRole != null) {
+            if (this.linkingConfig.isUseCustomRoles()) {
+                final long highestAllowedID = this.getHighestFromAllowed(member);
+
+                if (!this.linkingConfig.isOnlyCustomRoles()) {
+                    if (highestAllowedID != highestRole.getIdLong()) {
+                        return this.discordJDA.getColoredRole(highestRole);
+                    }
+                }
+
+                final String roleIcon = this.getRoleIcon(highestAllowedID);
+                return roleIcon == null ? "" : roleIcon;
+            }
+        }
+        return "";
+    }
+
+    private long getHighestFromAllowed(final Member member) {
+        for (final Role role : member.getRoles()) {
+            if (this.linkingConfig.getCustomRoles().containsKey(role.getIdLong())) {
+                return role.getIdLong();
+            }
+        }
+        return -1;
+    }
+
+    private String getRoleIcon(final long roleID) {
+        return this.linkingConfig.getCustomRoles().get(roleID);
     }
 }
